2738125b413b247a6e64d2cc91473b36f2c79389
Yehezkiel Reynaldi Abraham <yehezkielreynaldi01@gmail.com>
2023-05-02 20:39:19 +0700

    Tugas 22 JS
    Tugas ini saya kirimkan untuk Niomic

---
+++ tugas22.js
@@ -0,0 +1,11 @@
+function panggilForEach() {
+
+    let data = ['Saya', 'ingin', 'belajar', 'bersama'];
+
+    data.forEach(function (item, index) {
+        console.log(`Item : ${item} Index ke ${index}`);
+    });
+
+}
+
+panggilForEach();
